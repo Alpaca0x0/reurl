baea8dad205f75557ac5c30f85252b77ace27c7a
alpaca0x0 <alpaca0x0.tw@gmail.com>
2023-06-04 05:59:39 +0800

    for debug

--- routers/page.php
+++ routers/page.php
@@ -2,7 +2,8 @@
 Router::new(Path::page);
 
 Router::equal('/', function () { Router::redirect('/index/'); });
-Router::equal('/index/', function () { Router::view(); });
+Router::equal('/index/', function () { Router::view('index'); });
+if(DEBUG) Router::equal('/lab/', function () { Router::view('lab'); });
 Router::view('redirect');
 
 http_response_code(404);
