eaf52b464bdc87741b8e8a00bf1ea59edca516a2
alpaca0x0 <alpaca0x0.tw@gmail.com>
2023-06-06 06:25:54 +0800

    powered by ``cert.pl`

--- configs/black-domains/update.sh
+++ configs/black-domains/update.sh
@@ -4,4 +4,5 @@ output_file="domains.txt"
 
 mv $output_file $output_file.bak 2>/dev/null
 
+# from https://cert.pl/en/posts/2020/03/malicious_domains/
 curl https://hole.cert.pl/domains/domains.txt --output $output_file
